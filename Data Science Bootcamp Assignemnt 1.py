4c3af7fd4c3935583b5539efeff676501ea84a99
Anshuman Rehni <99117118+Anshuman08121999@users.noreply.github.com>
2022-05-26 14:40:45 +0530

    Add files via upload

---
+++ Data Science Bootcamp Assignemnt 1.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Q.1
+# * is an expression as it a used to do a mathematical operation (Multiplication).
+# 
+# 'hello' is a value of an element as values can be integer or string. 
+# 
+# -87.8 is a value of an element as values can be integer or string.
+# 
+# - is an expression as it a used to do a mathematical operation (Subtraction).
+# 
+# / is an expression as it a used to do a mathematical operation (Division).
+# 
+# + is an expression as it a used to do a mathematical operation (Addition).
+# 
+# 6 is a value of an element as values can be integer or string
+
+# # Q.2
+# Variables are symbols that you can use to store data in a program.
+# String is a type of information you would store in a Variable. 
+
+# # Q.3
+# Data Types     Examples          Explanation
+# Integers         5364              Whole numbers
+# Floats          3.1415             Decimal Numbers
+# Booleans       True, False       Truth values that represent Yes/No
+
+# # Q.4
+# An expression is a combination of operators and operands that is interpreted to produce some other value. In any programming language, an expression is evaluated as per the precedence of its operators.
+
+# # Q.5
+# Expression is made up of values, containers, and mathematical operators (operands) and the statement is just like a command that a python interpreter executes like print. In short – An Expression always evaluates to a value. And, A statement does something, like creating a variable or displaying a value, it only does whatever the statement says.
+
+# In[14]:
+
+
+#Q.6
+bacon=22
+bacon+1
+
+#After running this code variable bacon contains 23
+
+
+# In[17]:
+
+
+#Q.7
+"spam"+"spamspam"
+
+
+# In[18]:
+
+
+"spam"*3
+
+
+# In[19]:
+
+
+#values of the following two terms will be 'spamspamspam'.
+
+
+# # Q.8 
+# Egg is a valid variable name while 100 is invalid because Variable names cannot begin with a number as per python rules. A variable name can only contain alpha-numeric characters and underscores 
+
+# # Q.9
+# The int() , float() , and str( ) functions will evaluate to the integer, floating-point number, and string versions of the value passed to them.
+
+# # 10
+
+# In[21]:
+
+
+"i have eaten" + 99 + "burritos."
+
+
+# In[22]:
+
+
+#This expression cause an error because it cannot concatenate string with integer. Both expression or values needs to be in same Versions. We can fix it by doing a type casting operation as shown below:
+
+
+# In[27]:
+
+
+"i have eaten"+ str('99')+  "burritos."
+
+
+# In[ ]:
+
+
+
+
